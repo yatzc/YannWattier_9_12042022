7bbb1f2fd7af8faefc0b00eb8f83c80015f14996
yatzc <wattier.yann-81@hottmail.fr>
2022-05-26 11:51:59 +0200

    [Bug Hunt] - Dashboard

--- bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
+++ bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
@@ -85,7 +85,9 @@ export default class {
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
+  // [Bug Hunt] - Dashboard
   handleEditTicket(e, bill, bills) {
+    e.stopImmediatePropagation();
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
